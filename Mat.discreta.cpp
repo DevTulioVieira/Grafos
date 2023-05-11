8737b37bb6fccc11fd252d9bf9a50fcd63a4d37a
Túlio Vieira <72238565+DevTulioVieira@users.noreply.github.com>
2023-05-11 20:09:56 -0300

    Add files via upload

--- Mat.discreta.cpp
+++ Mat.discreta.cpp
@@ -2,14 +2,18 @@
 #include <iomanip>
 #include <windows.h>
 #include <fstream>
+#include <stdio.h>
 
 using namespace std;
+const int cols = 3;
 //Informacoes de cada ponto
 typedef struct matrizlista{
     int ponto;//numero do ponto
-    int verificado;//1 para se reservatorio 0 para somente ponto normal
+    int energia;//1 para se reservatorio 0 para somente ponto normal
+    int analisado;
     string nome;//nome para indentifica melho o ponto
     struct matrizlista*prox;
+    struct matrizlista*liga;
 }matriz;
 
 typedef struct lista{
@@ -22,17 +26,18 @@ void iniciomatriz(matrizL *lista){
     lista->fim=NULL;
 }
 
-
 void menu(){
     cout<<"========================================="<<endl;
     cout<<"=1.Calcular linhas cortadas para apagao ="<<endl;
+    cout<<"=2.Mostra pontos e suas ligacao         ="<<endl;
+    cout<<"=3.Mostra Pontos de energia             ="<<endl;
     cout<<"=4.Mostra os dados                      ="<<endl;
     cout<<"=5.Sair                                 ="<<endl;
     cout<<"========================================="<<endl;
 }
 //nessa funcao estou induzindo os ponto que existem puxando seus dados de pontos.txt edeixando eles
 //em uma lista encadeada para que estejam conectados e possam se removidos ou alterado
-void carregarpontos(matrizL *lista){
+void carregarpontos(matrizL *lista, int *tam){
     matriz *novo;
     novo = new matriz;
     novo->prox=NULL;
@@ -40,11 +45,12 @@ void carregarpontos(matrizL *lista){
     ifstream inFile;
     inFile.open("pontos.txt", ios::in);
     if (!inFile){ 
-        cout<<"Arquivo saida.txt nao pode ser aberto"<<endl;
+        cout<<"Arquivo pontos.txt nao pode ser aberto"<<endl;
         abort();
     }
-
-    while(inFile>>novo->ponto>>novo->verificado>>novo->nome){
+    *tam=0;
+    while(inFile>>novo->ponto>>novo->energia>>novo->nome){
+        novo->analisado=0;
         if (lista->inicio == NULL){
             lista->inicio = novo;
             lista->fim = novo;
@@ -54,10 +60,11 @@ void carregarpontos(matrizL *lista){
         }
         novo = new matriz;
         novo->prox = NULL;
+        *tam=*tam+1;
     }
 }
 //mostra as informacao de cada ponto
-void mostra(matrizL *lista){
+void mostraTotal(matrizL *lista){
     matriz *aux;
     aux=lista->inicio;
 
@@ -65,54 +72,89 @@ void mostra(matrizL *lista){
         cout<<"ERRO!"<<endl;
     }else{
         while(aux!=NULL){
-            cout<<"numero:     "<<aux->ponto<<endl;
-            cout<<"verificado: "<<aux->verificado<<endl;
+            cout<<"Ponto:      "<<aux->ponto<<endl;
+            cout<<"energia:    "<<aux->energia<<endl;
             cout<<"nome:       "<<aux->nome<<endl<<endl;
             aux=aux->prox;
         }
     }
 }
 
-void carregardados(matrizL *lista){
-    matriz *novo;
-    novo = new matriz;
-    novo->prox=NULL;
+void mostraEnergia(matrizL *lista){
+    matriz *aux;
+    aux=lista->inicio;
 
-    ifstream inFile;
-    inFile.open("dados.txt", ios::in);
-    if (!inFile){ 
-        cout<<"Arquivo saida.txt nao pode ser aberto"<<endl;
-        abort();
-    }
-
-    while(inFile>>novo->ponto>>novo->verificado>>novo->nome){
-        
-        novo = new matriz;
-        novo->prox = NULL;
+    if(aux==NULL){
+        cout<<"ERRO!"<<endl;
+    }else{
+        while(aux!=NULL){
+            if(aux->energia==1){
+                cout<<"Ponto:      "<<aux->ponto<<endl;
+                cout<<"nome:       "<<aux->nome<<endl<<endl;
+            }
+            aux=aux->prox;
+        }
     }
 }
 
+void mostraLiga(matrizL *lista, int vet[3]){
+    int i;
+    for(i=0; i<3; i++){
+        if(i==0){
+            vet[i]=3;
+        }
+        cout<<vet[i];
+    }
+    cout<<"\n";
+}
+
 int main(){
-    int opcao;
+    int opcao, tamanho=0, i, j;
 
     matrizL M1;
 
     iniciomatriz(&M1);
 
-    carregarpontos(&M1);
+    carregarpontos(&M1, &tamanho);
+    tamanho=3;
+    int mat[tamanho][tamanho];
+
+    mat[0][0]=0;
+    mat[0][1]=0;
+    mat[0][2]=0;
+    mat[1][0]=1;
+    mat[1][1]=1;
+    mat[1][2]=1;
+    mat[2][0]=0;
+    mat[2][1]=0;
+    mat[2][2]=0;
+
 
     for(;;){
+        system("cls");
         menu();
         cin>>opcao;
+        system("cls");
         switch(opcao){
             case 1:
 
             break;
-
-            case 4:
-                mostra(&M1);
+            case 2:
+                for(i=0; i<tamanho; i++){
+                    mostraLiga(&M1, mat[i]);
+                }
                 system("pause");
-                system("cls");
+                cout<<mat[0][0];
+                system("pause");
+
+            break;
+            case 3:
+                mostraEnergia(&M1);
+                system("pause");
+            break;
+            case 4:
+                mostraTotal(&M1);
+                system("pause");
             break;
 
             case 5:
